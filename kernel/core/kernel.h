feb5f8532be3cb90a1da48c35919f462c9a411ba
kazah-png <kazah.png@proton.me>
2026-09-06 07:52:02 +0200

    v6.5.145 - column: table-aligner port (util-linux column -t)

--- kernel/core/kernel.h
+++ kernel/core/kernel.h
@@ -18,7 +18,7 @@
 // Constantes
 // ============================================================
 #define KERNEL_NAME    "NyxOS"
-#define KERNEL_VERSION "6.5.144"
+#define KERNEL_VERSION "6.5.145"
 #define KERNEL_CODENAME "GUI Suite"
 #define KERNEL_DATE    "2026"
 
